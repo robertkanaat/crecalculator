0a2f0e9aaa92c03cdc5c8244cc0f0ec0955e85f5
Robert Kanaat <35250597+robertkanaat@users.noreply.github.com>
2026-03-08 13:10:59 -0400

    Add files via upload

--- vit.config.js
+++ vit.config.js
@@ -1,13 +1,8 @@
+// vite.config.js (in project root)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                        // important for correct paths on Amplify
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',                        // important for correct paths on Amplify
+  base: '/',  // Ensures correct asset paths on Amplify
 })
